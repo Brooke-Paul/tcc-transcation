6addcacf568f67e714613d5f6ad88f260192bc2d
薛滔 <gentle.taoxue@gmail.com>
2020-03-29 23:34:53 +0800

    处理服务设置

--- tcc-transcation-order/src/main/java/com/tcc/order/OrderApplication.java
+++ tcc-transcation-order/src/main/java/com/tcc/order/OrderApplication.java
@@ -1,9 +1,11 @@
 package com.tcc.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.tcc.order")
 public class OrderApplication {
 
     public static void main(String[] args) {
